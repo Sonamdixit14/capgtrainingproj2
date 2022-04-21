b46f3bef91fb94e8d1fc8a949d42f5dcf72e326b
Sonamdixit14 <100353915+Sonamdixit14@users.noreply.github.com>
2022-04-21 22:21:49 +0530

    Add files via upload

---
+++ FirstPrgramme.java
@@ -0,0 +1,10 @@
+class FirstProgramme{
+   public static void main(String []args){
+     System.out.print("********");
+     System.out.print("This is my first programme");
+     System.out.print("********\n");
+     System.out.print("*********");
+     System.out.print("This is demo on print");
+     System.out.print("*********");
+     }
+}
